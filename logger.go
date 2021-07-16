64143d69d6375826b188777985c71fb7f1be8c41
fengberlin <fengberlin@qq.com>
2021-07-16 19:21:38 +0800

    fix

--- logger.go
+++ logger.go
@@ -1,9 +1,47 @@
 package log
 
-import "go.uber.org/zap"
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"sync"
+)
 
-func NewLogger() (*zap.Logger, error) {
+type innerLogger struct {
+	logger        *zap.Logger
+	sugaredLogger *zap.SugaredLogger
+	once          sync.Once
+}
 
+const (
+	defaultTimeFormatLayout = "2006-01-02T15:04:05.000Z07:00"
+)
+
+// defaultTimeEncoder init a zap customized time encoder
+func defaultTimeEncoder() zapcore.TimeEncoder {
+	//enc.AppendString(t.Format(defaultTimeFormatLayout))
+	return zapcore.TimeEncoderOfLayout(defaultTimeFormatLayout)
+}
+
+func defaultEncoderConfig() zapcore.EncoderConfig {
+	cfg := zapcore.EncoderConfig{
+		TimeKey:          "ts",
+		LevelKey:         "level",
+		NameKey:          "logger",
+		CallerKey:        "caller",
+		FunctionKey:      zapcore.OmitKey,
+		MessageKey:       "msg",
+		StacktraceKey:    "stacktrace",
+		LineEnding:       zapcore.DefaultLineEnding,
+		EncodeLevel:      zapcore.LowercaseLevelEncoder,
+		EncodeTime:       defaultTimeEncoder(),
+		EncodeDuration:   zapcore.SecondsDurationEncoder,
+		EncodeCaller:     zapcore.ShortCallerEncoder,
+		ConsoleSeparator: "",
+	}
+	return cfg
+}
+
+func NewLogger(opts ...Option) (*zap.Logger, error) {
 	l := zap.New(nil)
 	return l, nil
 }
--- options.go
+++ options.go
@@ -1,6 +1,24 @@
 package log
 
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"time"
+)
+
 type options struct {
+	level zap.AtomicLevel
+	encoderCfg zapcore.EncoderConfig
+	console bool
+	serviceName string
+	fileName string
+	fileRotateMaxAge time.Duration
+	fileRotationTime time.Duration
+	development bool
+	addCaller bool
+	callSkip int
+	addStacktrace zapcore.LevelEnabler
+	wrapCoreFunc func(zapcore.Core) zapcore.Core
 }
 
 type Option interface {
@@ -17,4 +35,22 @@ func (fo *optionFunc) apply(opts *options) {
 
 func newOptionFunc(f func(*options)) *optionFunc {
 	return &optionFunc{f: f}
+}
+
+func WithLevel(l zapcore.Level) Option {
+	return newOptionFunc(func(o *options) {
+		o.level = zap.NewAtomicLevelAt(l)
+	})
+}
+
+func WithServiceName(serviceName string) Option {
+	return newOptionFunc(func(o *options) {
+		o.serviceName = serviceName
+	})
+}
+
+func WithFileName(fileName string) Option {
+	return newOptionFunc(func(o *options) {
+		o.fileName = fileName
+	})
 }
